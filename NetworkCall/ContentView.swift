6d2621bbec7784b135a6d9294f0436a4de4d4195
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2024-01-20 17:09:42 +0700

    Initial Commit

---
+++ NetworkCall/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkCall
+//
+//  Created by Hieu Le on 1/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkCall/NetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallApp.swift
+//  NetworkCall
+//
+//  Created by Hieu Le on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
